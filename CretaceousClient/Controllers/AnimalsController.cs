50b9b91f19bd91f231662eeb66085331e92261a4
Maxwell Meyer <maxreadswell@gmail.com>
2021-08-17 21:51:41 -0700

    begin building animals controller, index route

---
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CretaceousClient.Models;
+
+namespace CretaceousClient.Controllers
+{
+  public class AnimalsController : Controller
+  {
+    public IActionResult Index()
+    {
+      var allAnimals = Animal.GetAnimals();
+      return View(allAnimals);
+    }
+
+    [HttpPost]
+    public IActionResult Index(Animal animal)
+    {
+      Animal.Post(animal);
+      return RedirectToAction("Index");
+    }
+  }
+}
